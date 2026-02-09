2af8f49cff2067bd67f92f3471ad0024a6465ace
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-02-09 15:33:20 +0100

    adds ui modal ceo dashboard

--- client/src/components/admin_data/AdminModal.tsx
+++ client/src/components/admin_data/AdminModal.tsx
@@ -4,80 +4,84 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
 import { sendReminderWorker } from "@/lib/api";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import { useEffect, useState } from "react";
 
 type TCloseModal = {
-  onClose?: () => void;
+    onClose?: () => void;
 };
 
 const formSchema = z.object({
-  email: z.email(),
-  subject: z.string(),
-  test: z.string(),
+    email: z.email(),
+    subject: z.string(),
+    test: z.string(),
 });
 
 export type sendEmailSchema = z.infer<typeof formSchema>;
 
 function AdminModal({ onClose }: TCloseModal) {
-  const {
-    mutate: sendReminder,
-    isError,
-    isSuccess,
-  } = useMutation<unknown, Error, sendEmailSchema>({
-    mutationKey: ["employee_email"],
-    mutationFn: (data: sendEmailSchema) => sendReminderWorker(data),
-  });
+    const {
+        mutate: sendReminder,
+        isError,
+        isSuccess,
+    } = useMutation<unknown, Error, sendEmailSchema>({
+        mutationKey: ["employee_email"],
+        mutationFn: (data: sendEmailSchema) => sendReminderWorker(data),
+    });
 
-  const onSubmit: SubmitHandler<sendEmailSchema> = (data) => sendReminder(data);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<sendEmailSchema>({ resolver: zodResolver(formSchema) });
+    const onSubmit: SubmitHandler<sendEmailSchema> = (data) =>
+        sendReminder(data);
 
-  return (
-    <>
-      {/* <div
-        onClick={onClose}
-        className="h-screen inset-0 fixed z-40 bg-black/60"
-      ></div> */}
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<sendEmailSchema>({ resolver: zodResolver(formSchema) });
 
-      <div className="max-h-min mt-40 mx-auto text-center items-center z-50 bg-gray-200 rounded-xl w-2xl">
-        <div className="flex items-center gap-10 justify-center mb-6 m-10 ">
-          <form
-            className="flex flex-col w-md outline justify-center "
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <input placeholder="email" {...register("email")} />
-            {errors.email && <span>{errors.email.message}</span>}
-            <input placeholder="subject" {...register("subject")} />
-            <textarea
-              placeholder="schreibe hier"
-              {...register("test")}
-            ></textarea>
+    return (
+        <>
+            <div className="max-h-min mt-40 mx-auto text-center items-center z-50 bg-gray-200 rounded-xl w-2xl">
+                <div className="flex items-center gap-10 justify-center mb-6 m-10 ">
+                    <form
+                        className="flex flex-col gap-5 w-md outline justify-center "
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        <p>Email Vorlage</p>
+                        <Input placeholder="email" {...register("email")} />
+                        {errors.email && <span>{errors.email.message}</span>}
+                        <Input placeholder="subject" {...register("subject")} />
+                        <Textarea
+                            placeholder="schreibe hier"
+                            {...register("test")}
+                        ></Textarea>
 
-            {isSuccess && (
-              <div className="text-green-600">
-                <p>Email gesendet</p>
-              </div>
-            )}
+                        {isSuccess && (
+                            <div className="text-green-600">
+                                <p>Email gesendet</p>
+                            </div>
+                        )}
 
-            {isError && <div>Etwas ist schief gelaufen</div>}
+                        {isError && <div>Etwas ist schief gelaufen</div>}
 
-            {isSuccess ? (
-              <Button className="outline" onClick={onClose}>
-                Schliessen
-              </Button>
-            ) : (
-              <Button type="submit" className="">
-                Send email
-              </Button>
-            )}
-          </form>
-        </div>
-        <p>Email Vorlage</p>
-      </div>
-    </>
-  );
+                        <Button
+                            type={isSuccess ? "button" : "submit"}
+                            onClick={isSuccess ? onClose : undefined}
+                            className=""
+                        >
+                            {isSuccess ? (
+                                <span className="text-green-700">
+                                    Schliessen
+                                </span>
+                            ) : (
+                                <span>Senden</span>
+                            )}
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default AdminModal;
