79b57e30262727a5fd5ea6d5413e3ff69eea745a
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-02-24 16:06:06 +0100

    adds modal refractor

---
+++ client/src/components/home/HomeModal.tsx
@@ -0,0 +1,28 @@
+import { FormInputs } from '@/schemas/zodSchema';
+import { UseMutationResult } from '@tanstack/react-query';
+import Modal from '@/components/modal/Modal';
+
+type HomeModalProps = {
+  modal: boolean;
+  toggleModal: () => void;
+  onSubmit: UseMutationResult<any, Error, FormInputs, unknown>;
+};
+
+const HomeModal = ({ toggleModal, onSubmit, modal }: HomeModalProps) => {
+  return (
+    <>
+      {modal && (
+        <div className="fixed inset-0 z-50 flex">
+          <div
+            onClick={toggleModal}
+            className="fixed inset-0 bg-black/50 cursor-pointer"
+            aria-label="Close modal"
+          />
+          <Modal className="p-4 rounded-lg" onSuccess={onSubmit} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default HomeModal;
