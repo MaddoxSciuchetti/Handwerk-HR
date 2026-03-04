f026d36550062d727a870ca5dc68c5712d218952
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-03-04 17:46:01 +0100

    feat(WorkerForm): improve rendering performance by memoizing forminputs

---
+++ client/src/features/worker-lifecycle/consts/form.consts.ts
@@ -0,0 +1,37 @@
+export const Inputs = [
+  {
+    name: 'vorname' as const,
+    placeholder: 'Vorname',
+    required: true,
+  },
+  {
+    name: 'nachname' as const,
+    placeholder: 'Nachname',
+    required: false,
+  },
+  {
+    name: 'email' as const,
+    placeholder: 'Email',
+    required: false,
+  },
+  {
+    name: 'geburtsdatum' as const,
+    placeholder: 'Geburtsdatum DD.MM.YYYY',
+    required: false,
+  },
+  {
+    name: 'adresse' as const,
+    placeholder: 'Adresse',
+    required: false,
+  },
+  {
+    name: 'eintrittsdatum' as const,
+    placeholder: 'Eintrittsdatum DD.MM.YYYY',
+    required: false,
+  },
+  {
+    name: 'position' as const,
+    placeholder: 'Position',
+    required: false,
+  },
+];
