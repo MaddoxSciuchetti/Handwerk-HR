ec459e89995a013212951e0a22999ed42131e800
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-03-05 10:45:05 +0100

    refactor(WorkerFileUploads): extract fileupload & zip export into FileHeader component

--- client/src/features/task-management/components/modal/files/WorkerFileUploads.tsx
+++ client/src/features/task-management/components/modal/files/WorkerFileUploads.tsx
@@ -1,3 +1,5 @@
+import CenteredDiv from '@/components/alerts/layout-wrapper/CenteredDiv';
+import ModalOverlay from '@/components/modal/ModalOverlay';
 import useDeleteWorkerFiles from '@/features/task-management/hooks/use-deleteWorkerFiles';
 import useGetWorkerFiles from '@/features/task-management/hooks/use-getWorkerFiles';
 import handleZipExport from '@/features/task-management/utils/handleZipExport';
@@ -6,6 +8,7 @@ import { Button } from '../../../../../components/ui/button';
 import { Spinner } from '../../../../../components/ui/spinner';
 import { fileIcon, getFileName } from '../../../utils/fileHandling';
 import FileUpload01 from './file_upload/form-main';
+import FileHeader from './FileHeader';
 
 type WorkerFileUploadsProps = {
   id: number;
@@ -16,33 +19,24 @@ function WorkerFileUploads({ id }: WorkerFileUploadsProps) {
   const { deleteFiles } = useDeleteWorkerFiles(id);
   const { toggleModal, modal, setModal } = useToggleModal();
 
+  if (isLoading || isFetching)
+    return (
+      <CenteredDiv>
+        <Spinner />
+      </CenteredDiv>
+    );
+
   return (
     <>
       <div className="text-right ">
-        {isLoading || isFetching ? (
-          <div className="flex items-center justify-center min-h-100">
-            {' '}
-            <Spinner className="size-8" />
-          </div>
-        ) : !fetchFiles || fetchFiles.length === 0 ? (
+        {!fetchFiles || fetchFiles.length === 0 ? (
           <>
             <div>
-              <div className="flex flex-row justify-end pt-5 pr-5">
-                <img
-                  className=" flex flex-end cursor-pointer outline rounded-sm p-1"
-                  onClick={toggleModal}
-                  src="/assets/copy.svg"
-                  alt="Upload File"
-                />
-
-                <Button
-                  variant={'outline'}
-                  onClick={() => handleZipExport(fetchFiles)}
-                  className="ml-4 cursor-pointer p-1"
-                >
-                  Zip export
-                </Button>
-              </div>
+              <FileHeader
+                toggleModal={toggleModal}
+                handleZipExport={handleZipExport}
+                fetchFiles={fetchFiles}
+              />
               <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
                 {fetchFiles?.map((file, index) => (
                   <div
@@ -137,14 +131,9 @@ function WorkerFileUploads({ id }: WorkerFileUploadsProps) {
       </div>
 
       {modal && (
-        <div className="fixed inset-0 z-50 flex">
-          <div
-            onClick={toggleModal}
-            className="fixed inset-0 bg-black/50 cursor-pointer"
-            aria-label="Close modal"
-          />
+        <ModalOverlay handleToggle={toggleModal}>
           <FileUpload01 setModal={setModal} id={id} />
-        </div>
+        </ModalOverlay>
       )}
     </>
   );
