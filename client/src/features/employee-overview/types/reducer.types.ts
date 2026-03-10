d58d7c7bd38a0ca1c9981fe20c2ce44b315b5dfc
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-03-10 07:49:12 +0100

    add employeeCreate function to provider

--- client/src/features/employee-overview/types/reducer.types.ts
+++ client/src/features/employee-overview/types/reducer.types.ts
@@ -1,9 +1,11 @@
 export type ModalState =
   | { kind: 'closed' }
   | { kind: 'create' }
-  | { kind: 'edit'; employeeId: string; fullname: string };
+  | { kind: 'edit'; employeeId: string; fullname: string }
+  | { kind: 'employeecreate' };
 
 export type ModalAction =
   | { type: 'OPEN_CREATE' }
   | { type: 'OPEN_EDIT'; employeeId: string; fullname: string }
-  | { type: 'CLOSE' };
+  | { type: 'CLOSE' }
+  | { type: 'EMPLOYEE_CREATE' };
