0d4c0b670693fa89e2c63d80c4203f206b38fca3
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-03-15 16:59:50 +0100

    refactor password validation utils into own file

---
+++ client/src/features/auth/utils/passwordValidation.ts
@@ -0,0 +1,31 @@
+export const getStrength = (
+  password: string,
+  progressPercent: number,
+  fulfilledRules: number
+) => {
+  if (password.length === 0) {
+    return {
+      width: '0%',
+      barClass: 'bg-(--muted)',
+    };
+  }
+
+  if (fulfilledRules <= 2) {
+    return {
+      width: `${progressPercent}%`,
+      barClass: 'bg-(--destructive)',
+    };
+  }
+
+  if (fulfilledRules < 5) {
+    return {
+      width: `${progressPercent}%`,
+      barClass: 'bg-orange-500',
+    };
+  }
+
+  return {
+    width: '100%',
+    barClass: 'bg-green-600',
+  };
+};
