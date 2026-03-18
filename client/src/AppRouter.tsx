b3a37485ad427839d631417df544cf212e42ffa1
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-03-18 07:45:19 +0100

    remove console.log from AppRouter

--- client/src/AppRouter.tsx
+++ client/src/AppRouter.tsx
@@ -1,10 +1,10 @@
 import { RouterProvider } from '@tanstack/react-router';
+import LoadingAlert from './components/alerts/LoadingAlert';
 import useAuth from './features/user-profile/hooks/useAuth';
 import { router } from './router';
 function AppRouter() {
   const { user, isError, isLoading } = useAuth();
-  console.log({ user, isError, isLoading });
-  if (isLoading) return <p>hello</p>;
+  if (isLoading) return <LoadingAlert />;
   return (
     <RouterProvider
       router={router}
