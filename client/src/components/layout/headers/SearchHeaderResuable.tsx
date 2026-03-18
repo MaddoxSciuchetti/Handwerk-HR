eb1d44a6829666f85d8bbffb56543ab000b57c30
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-03-18 18:47:58 +0100

    add accessibility to SearchHeader

--- client/src/components/layout/headers/SearchHeaderResuable.tsx
+++ client/src/components/layout/headers/SearchHeaderResuable.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 type SearchHeaderResuableProps<T extends string | number> = {
   search?: T;
@@ -19,30 +20,30 @@ const SearchHeaderResuable = <T extends string | number>({
   const buttonLabel = description.trim();
 
   return (
-    <>
-      <div className="flex gap-5">
-        <Input
-          value={search}
-          onChange={(e) => setSearch?.(e.target.value as T)}
-          className="rounded-xl"
-          placeholder="Suche bei Namen"
-        />
-        <div className="flex gap-2 ">
-          <Button
-            variant={'outline'}
-            aria-label={buttonLabel}
-            onClick={(e) => {
-              e.stopPropagation();
-              action?.();
-              openModal();
-            }}
-            className="cursor-pointer rounded-xl hover:bg-accent hover:text-accent-foreground transition-colors hover:bg-(--hover-bg) hover:text-(--hover-foreground)"
-          >
-            {description}
-          </Button>
-        </div>
-      </div>
-    </>
+    <search className="flex gap-5">
+      <Label htmlFor="employee-search" className="sr-only">
+        Suche bei Namen
+      </Label>
+      <Input
+        id="employee-search"
+        value={search}
+        onChange={(e) => setSearch?.(e.target.value as T)}
+        className="rounded-xl"
+        placeholder="Suche bei Namen"
+      />
+      <Button
+        variant={'outline'}
+        aria-label={buttonLabel}
+        onClick={(e) => {
+          e.stopPropagation();
+          action?.();
+          openModal();
+        }}
+        className="cursor-pointer rounded-xl hover:bg-accent hover:text-accent-foreground transition-colors hover:bg-(--hover-bg) hover:text-(--hover-foreground)"
+      >
+        {description}
+      </Button>
+    </search>
   );
 };
 
