d4dc65e699f2f40f3f51b5f7ec090a47069fcbd9
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2026-03-18 07:50:40 +0100

    add user permission checking on employee-overview route

--- client/src/routes/employee-overview.tsx
+++ client/src/routes/employee-overview.tsx
@@ -1,8 +1,14 @@
 import EmployeeOverview from '@/features/employee-overview/components/EmployeeOverview';
 import { EmployeeModalProvider } from '@/features/employee-overview/context/ModalProvider';
-import { createFileRoute } from '@tanstack/react-router';
+import { RouterContext } from '@/router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/employee-overview')({
+  beforeLoad: ({ context }: { context: RouterContext }) => {
+    if (context.auth.user?.user_permission !== 'CHEF') {
+      throw redirect({ to: '/login' });
+    }
+  },
   component: RouteComponent,
 });
 
